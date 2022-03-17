3046330e2053752cdc6b6e3a1c115cfb9836e375
Hesam Arabzadeh <40280976+hesam-a@users.noreply.github.com>
2022-03-17 15:52:08 -0400

    Update wat_exchng_job.sh

--- wat_exchng_job.sh
+++ wat_exchng_job.sh
@@ -4,7 +4,7 @@ file_name="file"
 
 for i in `echo $file_name`; do
 
-#    vmd -dispdev text -e wat_exchng_counter.tcl -f ./${i} > ${i}_vmd.out
+    vmd -dispdev text -e wat_exchng_counter.tcl -f ./${i} > ${i}_vmd.out
 
     python ./wat_exchng_counter.py ${i}_vmd.out > ${i}_wat_exchng.out
 
